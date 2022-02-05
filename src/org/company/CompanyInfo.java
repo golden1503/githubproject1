0db2e321496ed32238bc76f0314506c1a364bb39
golden1503 <kamalesh620@gmail.com>
2022-02-05 19:49:49 +0530

    Sampletask

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,26 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		System.out.println("companyName:CTS");
+		// TODO Auto-generated method stub
+
+	}
+	private void companyId() {
+		System.out.println("companyId:001");
+		// TODO Auto-generated method stub
+
+	}
+	private void companyAddress() {
+		System.out.println("companyAddress:AAA,111,chennai");
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo a = new CompanyInfo();
+		a.companyName();
+		a.companyId();
+		a.companyAddress();
+	}
+
+}
