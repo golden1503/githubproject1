5142ea2d9020b00063e04072b37a86938361ef1d
golden1503 <kamalesh620@gmail.com>
2022-02-07 15:41:42 +0530

    location added

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -16,12 +16,6 @@ public class CompanyInfo {
 		// TODO Auto-generated method stub
 
 	}
-<<<<<<< HEAD
-	private void companylocation() {
-		System.out.println("company location: India");
-		// TODO Auto-generated method stub
-
-=======
 	private void companyPhno() {
 		System.out.println("companyPhno:044 41041055");
 	}
@@ -29,20 +23,27 @@ public class CompanyInfo {
 	private void companyEmailId() {
 		System.out.println("compnay email id:cts@global.com");
 		// TODO Auto-generated method stub
->>>>>>> 5f83e9624898322218d46a84e842e4299cdcb1d9
 	}
+	private void companylocation() {
+		System.out.println("company location: India");
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	
 	public static void main(String[] args) {
 		CompanyInfo a = new CompanyInfo();
 		a.companyName();
 		a.companyId();
 		a.companyAddress();
-<<<<<<< HEAD
-		a.companylocation();
-=======
 		a.companyPhno();
 		a.companyEmailId();
+		a.companylocation();
+
 		
->>>>>>> 5f83e9624898322218d46a84e842e4299cdcb1d9
+		
+
 	}
 
 }
